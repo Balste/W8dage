5e3121d119e76e3575a6787c6ae3dd1189dde09b
Balste <stev7833@gmail.com>
2025-07-15 15:46:04 +0200

    Update farm.gd

--- scripts/buildings/farm.gd
+++ scripts/buildings/farm.gd
@@ -1,1 +1,10 @@
+extends "res://Scripts/building.gd"
+
+func _ready():
+    resource_type = "food"
+    source_tile_ids = [4] # ID de la tuile "sol fertile"
+    worker_count = 0
+    max_workers = 4
+    bonus_radius = 5
+    ._ready()
 
