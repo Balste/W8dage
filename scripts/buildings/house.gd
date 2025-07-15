fd1ab859df3d2e9c7db636c8b09e09dc9f0e1081
Balste <stev7833@gmail.com>
2025-07-15 15:45:18 +0200

    Create house.gd

---
+++ scripts/buildings/house.gd
@@ -0,0 +1,9 @@
+extends "res://Scripts/building.gd"
+
+func _ready():
+    resource_type = ""
+    worker_count = 0
+    max_workers = 0
+    bonus_radius = 0
+    source_tile_ids = []
+    ._ready()
