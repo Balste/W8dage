bc0a7c6c8664fdc73dcffbea6ffcfb4f5b34b2a3
Balste <stev7833@gmail.com>
2025-07-15 15:36:01 +0200

    Create wood_camp.gd

---
+++ scripts/buildings/wood_camp.gd
@@ -0,0 +1,9 @@
+extends "res://Scripts/building.gd"
+
+func _ready():
+    resource_type = "wood"
+    source_tile_ids = [1]  # ID de la tuile "forest" dans ton terrain_tileset
+    worker_count = 0
+    max_workers = 4
+    bonus_radius = 5
+    ._ready()  # appel de la _ready() du parent pour l'enregistrement
