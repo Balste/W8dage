e975fbdb15a52901fa11a4159fbe3a6f465879ff
Balste <stev7833@gmail.com>
2025-07-15 14:17:31 +0200

    Create build_menu.gd

---
+++ UI/build_menu.gd
@@ -0,0 +1,34 @@
+extends Panel
+
+signal build_house
+signal build_wood_camp
+signal build_quarry
+signal build_mine
+signal build_farm
+signal build_well
+
+func _ready():
+	$VBoxContainer/HouseButton.pressed.connect(_on_house_pressed)
+	$VBoxContainer/WoodCampButton.pressed.connect(_on_wood_camp_pressed)
+	$VBoxContainer/QuarryButton.pressed.connect(_on_quarry_pressed)
+	$VBoxContainer/MineButton.pressed.connect(_on_mine_pressed)
+	$VBoxContainer/FarmButton.pressed.connect(_on_farm_pressed)
+	$VBoxContainer/WellButton.pressed.connect(_on_well_pressed)
+
+func _on_house_pressed():
+	emit_signal("build_house")
+
+func _on_wood_camp_pressed():
+	emit_signal("build_wood_camp")
+
+func _on_quarry_pressed():
+	emit_signal("build_quarry")
+
+func _on_mine_pressed():
+	emit_signal("build_mine")
+
+func _on_farm_pressed():
+	emit_signal("build_farm")
+
+func _on_well_pressed():
+	emit_signal("build_well")
