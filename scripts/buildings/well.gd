bba7f090b87f80454c3448dec845649ace15a313
Balste <stev7833@gmail.com>
2025-07-15 15:46:23 +0200

    Create well.gd

---
+++ scripts/buildings/well.gd
@@ -0,0 +1,9 @@
+extends "res://Scripts/building.gd"
+
+func _ready():
+    resource_type = "water"
+    source_tile_ids = [5] # ID de la tuile "eau" ou "rivière"
+    worker_count = 0
+    max_workers = 4
+    bonus_radius = 5
+    ._ready()
